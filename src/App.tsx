3d831df38be8313e377815c9485cb3a3c65e5439
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-17 16:52:12 -0300

    feat: module css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Fragment } from "react/jsx-runtime";
 
+import "./App.css"; // implements global css
+
 export function App() {
     return (
         <Fragment>
             <h1>Hello Heaven!</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+                odio commodi atque consequuntur animi illum tempore, voluptatem
+                tempora facilis deleniti vitae, doloremque ullam praesentium
+                labore veniam sint iusto deserunt, rerum ducimus. Eveniet omnis
+                fugit nihil sit dignissimos consequuntur inventore aliquid porro
+                dolores tenetur nulla ad placeat ut harum veritatis vitae,
+                nostrum dolore odio odit modi iste. Dolores itaque ducimus
+                mollitia iste dolore libero. Vel maxime optio a tenetur sunt
+                aperiam. Fugit possimus delectus suscipit dolores, sint libero
+                minima non sunt molestias, error necessitatibus quos quasi vel,
+                neque id provident cum? Quod aperiam excepturi eos itaque
+                officiis, repellendus id tenetur voluptate.
+            </p>
         </Fragment>
     );
 }
