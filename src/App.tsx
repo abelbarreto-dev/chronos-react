05fd10acc0ebd3619dc011d3e7b9b17edea45da8
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-18 10:10:03 -0300

    feat: basic html css structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,49 @@ import { TimerIcon } from "lucide-react";
 export function App() {
     return (
         <Fragment>
-            <Heading>
-                Hello World!
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                odio commodi atque consequuntur animi illum tempore, voluptatem
-                tempora facilis deleniti vitae, doloremque ullam praesentium
-                labore veniam sint iusto deserunt, rerum ducimus. Eveniet omnis
-                fugit nihil sit dignissimos consequuntur inventore aliquid porro
-                dolores tenetur nulla ad placeat ut harum veritatis vitae,
-                nostrum dolore odio odit modi iste. Dolores itaque ducimus
-                mollitia iste dolore libero. Vel maxime optio a tenetur sunt
-                aperiam. Fugit possimus delectus suscipit dolores, sint libero
-                minima non sunt molestias, error necessitatibus quos quasi vel,
-                neque id provident cum? Quod aperiam excepturi eos itaque
-                officiis, repellendus id tenetur voluptate.
-            </p>
+            <div className="container">
+                <div className="content">
+                    <section>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Itaque explicabo quos consequuntur possimus? Totam
+                        animi architecto maiores quaerat sint maxime quasi fugit
+                        reiciendis? Sed, non? Quae corrupti nisi ab accusamus.
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Itaque explicabo quos consequuntur possimus? Totam
+                        animi architecto maiores quaerat sint maxime quasi fugit
+                        reiciendis? Sed, non? Quae corrupti nisi ab accusamus.
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Itaque explicabo quos consequuntur possimus? Totam
+                        animi architecto maiores quaerat sint maxime quasi fugit
+                        reiciendis? Sed, non? Quae corrupti nisi ab accusamus.
+                    </section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Itaque explicabo quos consequuntur possimus? Totam
+                        animi architecto maiores quaerat sint maxime quasi fugit
+                        reiciendis? Sed, non? Quae corrupti nisi ab accusamus.
+                    </section>
+                </div>
+            </div>
         </Fragment>
     );
 }
