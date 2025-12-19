41fdc3d517dd7b0b2f7b8c3c9b2b57aeb886bacb
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-19 15:57:21 -0300

    feat: footer alement and links

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { Form } from "./components/Form";
+import { Footer } from "./components/Footer";
 
 import "./styles/theme.css";
 import "./styles/global.css"
-import { Form } from "./components/Form";
 
 export function App() {
     return (
@@ -25,6 +26,10 @@ export function App() {
 
             <Container>
                 <Form />
+            </Container>
+
+            <Container>
+                <Footer />
             </Container>
         </Fragment>
     );
