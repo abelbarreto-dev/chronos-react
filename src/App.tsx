733f16af91f05d0ecdfe2c38c298ae04a587a6b5
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-22 14:46:30 -0300

    feat: pomodoro and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,4 @@ import { Home } from "./pages/Home";
 import "./styles/theme.css";
 import "./styles/global.css";
 
-export function App() {
-    return <Home />;
-}
+export const App = () => (<Home />);
