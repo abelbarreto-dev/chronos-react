f35fe3107742e5ed91de0d066798fc4ee58e65f9
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-24 12:05:17 -0300

    feat: retraive code from before reducer example

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useState } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 import { initialTaskStateData } from "./initialTaskData";
 import { TaskContext } from "./TaskContext";
@@ -8,20 +8,9 @@ type TaskContextProviderProps = { children: React.ReactNode };
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
     const [state, setState] = useState<TaskStateModel>(initialTaskStateData);
 
-    const [numero, dispatch] = useReducer((state, action) => {
-        switch(action) {
-            case "INCREMENT": {
-                return state + 1;
-            }
-        }
-
-        return state;
-    }, 0);
-
     return (
         <TaskContext.Provider value={{ state, setState }}>
-            <h1>O Número é: {numero}</h1>
-            <button onClick={() => dispatch("INCREMENT")}>Incrementar</button>
+            {children}
         </TaskContext.Provider>
     );
 };
